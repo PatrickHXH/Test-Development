0f9d39c6608086bfc749fcb8f600222231bb19e3
PatrickHXH <864927796@qq.com>
2022-03-17 11:53:02 +0800

    upgrade

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 # module 对应的数据库ORM
-
-
+#python语言  ---> 数据库驱动 --->SQLite3 (sql语言)
+#ORM  把写SQL语句给我们屏蔽掉，可以像操作类方法一样操作数据库
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
