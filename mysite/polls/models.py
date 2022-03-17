d0f701baf41f52d3100fe9cd10d1cfe3adab1064
PatrickHXH <864927796@qq.com>
2022-03-17 18:09:12 +0800

    upgrade

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -22,7 +22,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+
     def __str__(self):
         return  self.question_text
 
