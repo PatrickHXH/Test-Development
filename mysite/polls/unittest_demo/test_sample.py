44acc60cb496ac827743e5baf41894354f7c1b23
PatrickHXH <864927796@qq.com>
2022-03-21 16:22:19 +0800

    upgrade

---
+++ mysite/polls/unittest_demo/test_sample.py
@@ -0,0 +1,13 @@
+# import unittest
+#
+# def setUpModule():
+#     print("===模块结束")
+# def tearDownModule():
+#     print("===模块结束")
+# class MyTest(unittest.TestCase):
+#
+#     def test_01(self):
+#         print("测试")
+#
+# if __name__ == '__main__':
+#     unittest.main()
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),     #<int:question_id>类似与正则表达式用外键id表示
     # ex: /polls/5/results/
-    path('<int:pk>', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
